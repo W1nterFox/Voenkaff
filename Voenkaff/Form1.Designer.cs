31b5f277dd9016d51b10d3f110694e9de50dfadb
W1nterFox <aleks23041996@gmail.com>
2018-01-06 21:38:49 +0400

    Tree of tasks

--- Voenkaff/Form1.Designer.cs
+++ Voenkaff/Form1.Designer.cs
@@ -29,29 +29,47 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.panelQuestionStart = new System.Windows.Forms.Panel();
+            this.panelAnswerStart = new System.Windows.Forms.Panel();
+            this.panelListOfTasks = new System.Windows.Forms.Panel();
+            this.panelTaskStart = new System.Windows.Forms.Panel();
+            this.buttonTaskCreate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panelMiddle = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
+            this.panelListOfTasks.SuspendLayout();
+            this.panelTaskStart.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Khaki;
+            this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(885, 0);
+            this.panel1.Location = new System.Drawing.Point(1275, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 642);
+            this.panel1.Size = new System.Drawing.Size(122, 642);
             this.panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Добавить надпись";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(49, 130);
+            this.button2.Location = new System.Drawing.Point(23, 114);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(87, 49);
             this.button2.TabIndex = 1;
@@ -61,7 +79,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(49, 34);
+            this.button1.Location = new System.Drawing.Point(23, 43);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(87, 52);
             this.button1.TabIndex = 0;
@@ -69,50 +87,95 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // panel2
+            // panelQuestionStart
             // 
-            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.panel2.Location = new System.Drawing.Point(13, 13);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(866, 490);
-            this.panel2.TabIndex = 1;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.panelQuestionStart.AutoScroll = true;
+            this.panelQuestionStart.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panelQuestionStart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelQuestionStart.Location = new System.Drawing.Point(5, 5);
+            this.panelQuestionStart.Name = "panelQuestionStart";
+            this.panelQuestionStart.Size = new System.Drawing.Size(1100, 632);
+            this.panelQuestionStart.TabIndex = 1;
+            this.panelQuestionStart.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
-            // button3
+            // panelAnswerStart
             // 
-            this.button3.Location = new System.Drawing.Point(49, 197);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(87, 41);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Добавить надпись";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.panelAnswerStart.AutoScroll = true;
+            this.panelAnswerStart.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.panelAnswerStart.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelAnswerStart.Location = new System.Drawing.Point(165, 524);
+            this.panelAnswerStart.Name = "panelAnswerStart";
+            this.panelAnswerStart.Size = new System.Drawing.Size(1110, 118);
+            this.panelAnswerStart.TabIndex = 3;
             // 
-            // panel4
+            // panelListOfTasks
             // 
-            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel4.AutoScroll = true;
-            this.panel4.BackColor = System.Drawing.Color.LightGoldenrodYellow;
-            this.panel4.Location = new System.Drawing.Point(13, 509);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(866, 121);
-            this.panel4.TabIndex = 3;
+            this.panelListOfTasks.AutoScroll = true;
+            this.panelListOfTasks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panelListOfTasks.Controls.Add(this.buttonTaskCreate);
+            this.panelListOfTasks.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelListOfTasks.Location = new System.Drawing.Point(0, 0);
+            this.panelListOfTasks.Name = "panelListOfTasks";
+            this.panelListOfTasks.Size = new System.Drawing.Size(165, 642);
+            this.panelListOfTasks.TabIndex = 4;
+            // 
+            // panelTaskStart
+            // 
+            this.panelTaskStart.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panelTaskStart.Controls.Add(this.panelQuestionStart);
+            this.panelTaskStart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelTaskStart.Location = new System.Drawing.Point(165, 0);
+            this.panelTaskStart.Margin = new System.Windows.Forms.Padding(10);
+            this.panelTaskStart.Name = "panelTaskStart";
+            this.panelTaskStart.Padding = new System.Windows.Forms.Padding(5);
+            this.panelTaskStart.Size = new System.Drawing.Size(1110, 642);
+            this.panelTaskStart.TabIndex = 5;
+            // 
+            // buttonTaskCreate
+            // 
+            this.buttonTaskCreate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonTaskCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonTaskCreate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonTaskCreate.Location = new System.Drawing.Point(29, 12);
+            this.buttonTaskCreate.Name = "buttonTaskCreate";
+            this.buttonTaskCreate.Size = new System.Drawing.Size(107, 30);
+            this.buttonTaskCreate.TabIndex = 1;
+            this.buttonTaskCreate.Text = "Новое задание";
+            this.buttonTaskCreate.UseVisualStyleBackColor = false;
+            this.buttonTaskCreate.Click += new System.EventHandler(this.buttonTaskCreate_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 373);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // panelMiddle
+            // 
+            this.panelMiddle.BackColor = System.Drawing.Color.White;
+            this.panelMiddle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMiddle.Location = new System.Drawing.Point(165, 0);
+            this.panelMiddle.Name = "panelMiddle";
+            this.panelMiddle.Size = new System.Drawing.Size(1110, 642);
+            this.panelMiddle.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1085, 642);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel2);
+            this.ClientSize = new System.Drawing.Size(1397, 642);
+            this.Controls.Add(this.panelAnswerStart);
+            this.Controls.Add(this.panelTaskStart);
+            this.Controls.Add(this.panelMiddle);
+            this.Controls.Add(this.panelListOfTasks);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panelListOfTasks.ResumeLayout(false);
+            this.panelTaskStart.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -122,9 +185,14 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelQuestionStart;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panelAnswerStart;
+        private System.Windows.Forms.Panel panelListOfTasks;
+        private System.Windows.Forms.Panel panelTaskStart;
+        private System.Windows.Forms.Button buttonTaskCreate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panelMiddle;
     }
 }
 
--- Voenkaff/Form1.cs
+++ Voenkaff/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -10,10 +12,73 @@ namespace Voenkaff
     public partial class Form1 : Form
     {
         private int _identifier = 1;
+        List<Panel> listPanelsTasks;
+
+        Panel currentTask = new Panel();
+        Panel currentPanelQuestion = new Panel();
+        Panel currentPanelAnswer = new Panel();
+
         public Form1()
         {
             InitializeComponent();
+
+            panelMiddle.Controls.Add(panelTaskStart);
+            listPanelsTasks = new List<Panel>();
+            listPanelsTasks.Add(panelTaskStart);
+
+            panelTaskStart.Controls.Add(panelQuestionStart);
+            panelTaskStart.Controls.Add(panelAnswerStart);
+            
+            panelAnswerStart.BringToFront();
+            //panelTask.Visible = false;
+
+            currentPanelQuestion = panelQuestionStart;
+            currentPanelAnswer = panelAnswerStart;
+
+            panelListOfTasks.Controls.Add(createLinkLabel(0));
+            
+
         }
+
+        private LinkLabel createLinkLabel(int indexPanel)
+        {
+            LinkLabel LL = new LinkLabel();
+            
+
+            LL.AutoSize = true;
+            LL.Font = new Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            LL.LinkColor = Color.Black;
+            LL.Location = new Point(10, 57 + indexPanel * 30);
+            LL.Name = "linkLabel" + indexPanel;
+            LL.Size = new Size(96, 16);
+            LL.TabIndex = 0;
+            LL.TabStop = true;
+            LL.Text = "Задание №" + (indexPanel + 1);
+            LL.VisitedLinkColor = Color.Black;
+            LL.Click += clickTaskLinkLabel;
+            LL.Tag = indexPanel;
+
+            return LL;
+        }
+
+        private void clickTaskLinkLabel(object sender, EventArgs e)
+        {
+            LinkLabel currentLL = (LinkLabel)sender;
+            
+            foreach (Panel index in listPanelsTasks)
+            {
+                index.Visible = false;
+            }
+            textBox1.Text = currentLL.Text;
+            listPanelsTasks[(int)currentLL.Tag].Visible = true;
+
+
+            currentTask = listPanelsTasks[(int)currentLL.Tag];
+            currentPanelQuestion = (Panel)currentTask.Controls.Find("panelQuestion", false)[0];
+            currentPanelAnswer = (Panel)currentTask.Controls.Find("panelAnswer", false)[0];
+
+        }
+
 
         private void panel2_Paint(object sender, PaintEventArgs e)
         {      
@@ -21,7 +86,7 @@ namespace Voenkaff
 
         private void button1_Click(object sender, EventArgs e)
         {
-            TextContainer tc = new TextContainer(panel2,panel4,this,_identifier);
+            TextContainer tc = new TextContainer(currentPanelQuestion, currentPanelAnswer, this,_identifier);
             _identifier++;
         }
 
@@ -32,7 +97,7 @@ namespace Voenkaff
 
         private void button2_Click(object sender, EventArgs e)
         {
-            PictureBoxScalable pb = new PictureBoxScalable(_identifier, this, panel2) {Pb = {Parent = panel2,SizeMode = PictureBoxSizeMode.StretchImage}};
+            PictureBoxScalable pb = new PictureBoxScalable(_identifier, this, currentPanelQuestion) {Pb = {Parent = currentPanelQuestion, SizeMode = PictureBoxSizeMode.StretchImage}};
             _identifier++;
 
 
@@ -62,13 +127,59 @@ namespace Voenkaff
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Title ttl = new Title(panel2,this,_identifier);
+            Title ttl = new Title(currentPanelQuestion, this,_identifier);
             _identifier++;
             ttl.ObjectEntity.BackColor = Color.Cyan;
             ttl.ObjectEntity.Font = new Font("Times New Roman",14f);
             ttl.ObjectEntity.Width=500;
         }
 
+        private void buttonTaskCreate_Click(object sender, EventArgs e)
+        {
+            listPanelsTasks[listPanelsTasks.Count - 1].Visible = false;
 
+            Panel newPanelTask = new Panel();
+
+            panelMiddle.Controls.Add(newPanelTask);
+            newPanelTask.BackColor = SystemColors.ControlLight;
+
+            Panel panelQuestion = new Panel();
+            panelQuestion.AutoScroll = true;
+            panelQuestion.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            panelQuestion.Dock = System.Windows.Forms.DockStyle.Fill;
+            panelQuestion.Location = new System.Drawing.Point(5, 5);
+            panelQuestion.Size = new System.Drawing.Size(1132, 632);
+            panelQuestion.Name = "panelQuestion";
+
+            Panel panelAnswer = new Panel();
+            panelAnswer.AutoScroll = true;
+            panelAnswer.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            panelAnswer.Dock = System.Windows.Forms.DockStyle.Bottom;
+            panelAnswer.Location = new System.Drawing.Point(133, 524);
+            panelAnswer.Size = new System.Drawing.Size(1142, 118);
+            panelAnswer.Name = "panelAnswer";
+
+            newPanelTask.Controls.Add(panelQuestion);
+            newPanelTask.Controls.Add(panelAnswer);
+            newPanelTask.Dock = DockStyle.Fill;
+            newPanelTask.Location = new Point(133, 0);
+            newPanelTask.Margin = new Padding(10);
+            newPanelTask.Padding = new Padding(5);
+            newPanelTask.Size = new Size(1142, 642);
+
+            
+            listPanelsTasks.Add(newPanelTask);
+            
+            listPanelsTasks[listPanelsTasks.Count - 1].Visible = true;
+
+
+            currentTask = listPanelsTasks[listPanelsTasks.Count - 1];
+            currentPanelQuestion = (Panel)currentTask.Controls.Find("panelQuestion", false)[0];
+            currentPanelAnswer = (Panel)currentTask.Controls.Find("panelAnswer", false)[0];
+
+            panelListOfTasks.Controls.Add(createLinkLabel(listPanelsTasks.Count - 1));
+
+
+        }
     }
 }
